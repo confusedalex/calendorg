76afcaf7ce33d049934bb1e62d894e7350b60dd7
confusedalex <alex@confusedalex.dev>
2025-05-27 17:03:02 +0200

    build(flake): switch to flakeutils

--- flake.nix
+++ flake.nix
@@ -2,37 +2,35 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
-    # treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-
-      imports = [
-        # inputs.flake-parts.flakeModules.partitions
-        # inputs.treefmt-nix.flakeModule
-      ];
-
-      perSystem =
-        {
-          system,
-          pkgs,
-          inputs',
-          ...
-        }:
-        {
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              flutter
-              pkg-config
-              zenity
-              dart
-            ];
-          };
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem nixpkgs.lib.systems.flakeExposed (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.android_sdk.accept_license = true;
+          config.allowUnfree = true;
         };
-    };
+      in
+      {
+        packages = flake-utils.lib.flattenTree { inherit (pkgs) hello; };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            flutter
+            pkg-config
+            zenity
+            dart
+            jdk17
+          ];
+        };
+      }
+    );
 }
