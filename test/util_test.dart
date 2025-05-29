72070273563ff6d55b74eb6da763c83c3812e6c8
confusedalex <alex@confusedalex.dev>
2025-05-29 22:37:55 +0200

    test(util): add event length test

---
+++ test/util_test.dart
@@ -0,0 +1,22 @@
+import 'package:calendorg/util.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:org_parser/org_parser.dart';
+
+void main() {
+  final markup = """
+* Heading 1
+** orgmode meetup
+<2025-05-05>
+<2025-05-06 11:00>
+<2025-05-08 11:00-13:00>
+<2025-05-28> <2025-05-15>
+<2025-05-01>--<2025-05-02>
+""";
+
+  test("Events lenth should be 1", () {
+    final document = OrgDocument.parse(markup);
+    final events = parseEvents(document);
+
+    expect(events.length, 1);
+  });
+}
