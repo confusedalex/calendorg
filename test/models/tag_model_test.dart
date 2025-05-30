9d60005cc39a79019059a7459be75df6971ac348
confusedalex <alex@confusedalex.dev>
2025-05-30 12:25:49 +0200

    test(tag_model): add tests for tag model

---
+++ test/models/tag_model_test.dart
@@ -0,0 +1,41 @@
+import 'dart:collection';
+import 'dart:convert';
+
+import 'package:calendorg/models/tag_model.dart';
+import 'package:calendorg/tag_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final schoolTagColor = TagColor("school", Colors.orange);
+
+Future<void> main() async {
+  Future<TagColorsModel> getTagColorsModel() async {
+    SharedPreferences.setMockInitialValues({
+      "tagColors": jsonEncode([schoolTagColor])
+    });
+
+    var tagColorsModel = TagColorsModel();
+
+    // wait for async to finish, I don't know how to do this better :/
+    await Future.delayed(Duration(milliseconds: 10));
+
+    return tagColorsModel;
+  }
+
+  test("Tags will be loaded from shared preferences", () async {
+    final tagColorsModel = await getTagColorsModel();
+
+    expect(tagColorsModel.tagColorsFromPrefs.first.toString(),
+        equals(schoolTagColor.toString()));
+  });
+
+  test("Add tag to model will add to list and save to prefs", () async {
+    final tagColorsModel = await getTagColorsModel();
+    final newTag = TagColor("new green tag", Colors.green);
+
+    tagColorsModel.addTagColor(TagColor("new green tag", Colors.green));
+    expect(tagColorsModel.tagColorsFromPrefs,
+        containsAll([schoolTagColor, newTag]));
+  });
+}
