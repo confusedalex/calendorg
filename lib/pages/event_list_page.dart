79c223b3d1f4e42096fbf8a2a7f7b8b012d21cc5
confusedalex <alex@confusedalex.dev>
2025-06-02 01:16:03 +0200

    feat: improve event list page

--- lib/pages/event_list_page.dart
+++ lib/pages/event_list_page.dart
@@ -1,8 +1,16 @@
+import 'package:calendorg/models/document_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:org_parser/org_parser.dart';
+import 'package:pretty_diff_text/pretty_diff_text.dart';
 
-Widget eventListPage(String display) => Center(
-  child: SingleChildScrollView(
-    scrollDirection: Axis.vertical,
-    child: Text(display),
-  ),
-);
+Widget eventListPage(String firstRead) => Center(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: BlocBuilder<OrgDocumentCubit, OrgDocument>(
+          // builder: (context, state) => Text(state.toMarkup()),
+          builder: (context, state) =>
+              PrettyDiffText(oldText: firstRead, newText: state.toMarkup()),
+        ),
+      ),
+    );
