1ec056bb6a0d2adf48b527de8c1c412626939c0e
confusedalex <alex@confusedalex.dev>
2025-06-02 16:24:24 +0200

    test(document_model): add tests

---
+++ test/models/document_model_test.dart
@@ -0,0 +1,37 @@
+import 'package:calendorg/models/document_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:org_parser/org_parser.dart';
+
+void main() {
+  final markup = """
+* Heading 1
+** orgmode meetup
+<2025-05-05>
+<2025-05-06 11:00>
+<2025-05-08 11:00-13:00>
+<2025-05-28> <2025-05-15>
+<2025-05-01>--<2025-05-03>
+** School :school:
+<2025-05-27>
+""";
+  late OrgDocument document;
+  setUp(() {
+    document = OrgDocument.parse(markup);
+  });
+
+  test("Cubit will accept and store OrgDocument", () {
+    var cubit = OrgDocumentCubit(document);
+
+    expect(cubit.state, equals(document));
+  });
+
+  test("Cubit will update document", () {
+    var cubit = OrgDocumentCubit(document);
+    final newOrgDocument = OrgDocument.parse("* Heading");
+
+    cubit.setDocument(newOrgDocument);
+
+    expect(cubit.state, equals(newOrgDocument));
+    expect(cubit.state, isNot(equals(document)));
+  });
+}
