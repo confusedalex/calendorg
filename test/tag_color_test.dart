c1afb924d46b8509e741e1be7f19e9604586989b
confusedalex <alex@confusedalex.dev>
2025-06-03 00:45:12 +0200

    test(TagColor): add equality test

--- test/tag_color_test.dart
+++ test/tag_color_test.dart
@@ -15,6 +15,12 @@ void main() {
               "TagColor(tag: @home, color: MaterialColor(primary value: Color(alpha: 1.0000, red: 0.2980, green: 0.6863, blue: 0.3137, colorSpace: ColorSpace.sRGB)))");
         },
       );
+      test("Two equal TagColors should be equal", () {
+        final TagColor a = TagColor("@home", Colors.green);
+        final TagColor b = TagColor("@home", Colors.green);
+        expect(a, equals(b));
+        expect(a.hashCode, b.hashCode);
+      });
     },
   );
 }
