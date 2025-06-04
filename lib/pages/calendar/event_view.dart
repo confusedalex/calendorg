1b9520fb0c4afbcb7f57cf895dad446fce4520e2
confusedalex <alex@confusedalex.dev>
2025-06-04 14:41:53 +0200

    wip(event_view): init eventview

--- lib/pages/calendar/event_view.dart
+++ lib/pages/calendar/event_view.dart
@@ -15,30 +15,68 @@ class EventView extends StatefulWidget {
 }
 
 class _EventViewState extends State<EventView> {
+  final _formKey = GlobalKey<FormState>();
   String newTitle = "";
+  bool isSavable() => _formKey.currentState?.validate() ?? true;
 
   @override
-  Widget build(BuildContext context) => AlertDialog(
-        title: Row(
-          children: [Text("Edit Event"), Spacer(), CloseButton()],
-        ),
-        content: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(border: OutlineInputBorder()),
-              initialValue: widget.event.title,
-              onChanged: (value) => newTitle = value,
-            )
-          ],
-        ),
-        actions: [
-          TextButton(
-              onPressed: () {
-                var oldNode = widget.event.section;
-                context.read<OrgDocumentCubit>().replaceNode(
-                    oldNode, changeSectionTitle(oldNode, newTitle));
-              },
-              child: Text("save"))
-        ],
-      );
+  Widget build(BuildContext context) =>
+      BlocBuilder<OrgDocumentCubit, OrgDocument>(builder: (context, state) {
+        return AlertDialog(
+            title: Row(
+              children: [Text("Edit Event"), Spacer(), CloseButton()],
+            ),
+            content: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    key: Key("TitleField"),
+                    decoration: InputDecoration(border: OutlineInputBorder()),
+                    initialValue: widget.event.title,
+                    autovalidateMode: AutovalidateMode.always,
+                    onChanged: (value) => (newTitle = value),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "The Event name can't be empty";
+                      }
+                      return null;
+                    },
+                  ),
+                  switch (widget.timestamp) {
+                    OrgSimpleTimestamp() => TextButton(
+                        onPressed: () => showDatePicker(
+                            context: context,
+                            firstDate: DateTime(0),
+                            lastDate: DateTime(3000),
+                            currentDate:
+                                (widget.timestamp as OrgSimpleTimestamp)
+                                    .dateTime),
+                        child: Text(widget.timestamp.toMarkup())),
+                    OrgDateRangeTimestamp() => TextButton(
+                        onPressed: () => showDateRangePicker(
+                            context: context,
+                            firstDate: DateTime(0),
+                            lastDate: DateTime(3000)),
+                        child: Text(widget.timestamp.toMarkup())),
+                    OrgTimeRangeTimestamp() => throw UnimplementedError(),
+                  },
+                  TextButton(
+                      key: Key("SaveButton"),
+                      onPressed: isSavable()
+                          ? () {
+                              final oldNode = widget.event.section;
+                              final newNode =
+                                  changeSectionTitle(oldNode, newTitle);
+                              context
+                                  .read<OrgDocumentCubit>()
+                                  .replaceNode(oldNode, newNode);
+                              Navigator.pop(context);
+                            }
+                          : null,
+                      child: Text("save"))
+                ],
+              ),
+            ));
+      });
 }
