d0966d535d54a3f0c72d2fb7ae0c2aaecc7897fc
confusedalex <alex@confusedalex.dev>
2025-06-04 19:43:08 +0200

    style(event_markers): remove unused imports

--- lib/features/calendar/event_markers.dart
+++ lib/features/calendar/event_markers.dart
@@ -2,7 +2,6 @@ import 'package:calendorg/event.dart';
 import 'package:calendorg/models/tag_model.dart';
 import 'package:calendorg/tag_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EventMarkers extends StatelessWidget {
