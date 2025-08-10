f1f09f52b53d3accf9629bf837a44bd4595e1298
confusedalex <alex@confusedalex.dev>
2025-08-10 19:39:39 +0200

    test: add tests for todo states cubit

---
+++ test/core/todo_states_cubit_test.dart
@@ -0,0 +1,73 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:test/test.dart';
+import 'package:calendorg/core/todo_states_cubit.dart';
+
+void main() {
+  group(
+    "todo_states_cubit_test",
+    () {
+      setUp(() {
+        SharedPreferences.setMockInitialValues({});
+      });
+
+      group("adding states", () {
+        test("adding TODO state works", () {
+          final cubit = TodoStatesCubit();
+
+          cubit.addTodo("todo", "TOCALL");
+
+          expect(cubit.state.todo, contains("TOCALL"));
+        });
+
+        test("adding DONE state works", () {
+          final cubit = TodoStatesCubit();
+
+          cubit.addTodo("done", "KILL");
+
+          expect(cubit.state.done, contains("KILL"));
+        });
+      });
+      group("removing states", () {
+        test("removing TODO state works", () {
+          final cubit = TodoStatesCubit();
+
+          cubit.addTodo("todo", "TOCALL");
+          cubit.removeTodo("todo", "TOCALL");
+
+          expect(cubit.state.todo, isNot(contains("TOCALL")));
+        });
+
+        test("removing DONE state works", () {
+          final cubit = TodoStatesCubit();
+
+          cubit.addTodo("done", "KILL");
+          cubit.removeTodo("done", "KILL");
+
+          expect(cubit.state.done, isNot(contains("KILL")));
+        });
+      });
+      group("loading from sharedPreferences", () {
+        getCubit() async {
+          SharedPreferences.setMockInitialValues(
+              {"todoStates": '["TOREAD"]', "doneStates": '["COMPLETED"]'});
+
+          final cubit = TodoStatesCubit()..loadFromPrefs();
+
+          await Future.delayed(Duration(milliseconds: 10));
+          return cubit;
+        }
+
+        test("loading TODO state from sharedPreferences works", () async {
+          final cubit = await getCubit();
+
+          expect(cubit.state.todo, contains("TOREAD"));
+        });
+        test("loading DONE state from sharedPreferences works", () async {
+          final cubit = await getCubit();
+
+          expect(cubit.state.done, contains("COMPLETED"));
+        });
+      });
+    },
+  );
+}
