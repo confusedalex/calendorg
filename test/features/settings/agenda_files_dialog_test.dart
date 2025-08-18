399f55e7927f6edcb9e4f614a935947d40a6bf37
confusedalex <alex@confusedalex.dev>
2025-08-18 22:56:11 +0200

    test: add sime test for agendaFilesDialog

---
+++ test/features/settings/agenda_files_dialog_test.dart
@@ -0,0 +1,40 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:calendorg/core/files/bloc/org_files_bloc.dart';
+import 'package:calendorg/features/settings/agenda_files_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
+
+void main() {
+  group(
+    "Agenda Files Dialog",
+    () {
+      setUp(() {
+        SharedPreferencesAsyncPlatform.instance =
+            InMemorySharedPreferencesAsync.empty();
+      });
+
+      Future<void> pumpWidgetToTester(dynamic tester) async {
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: BlocProvider(
+              create: (context) => OrgFilesBloc(),
+              child: AgendaFilesDialog(),
+            ),
+          ),
+        ));
+      }
+
+      testWidgets(
+        'should find add button',
+        (tester) async {
+          await pumpWidgetToTester(tester);
+
+          expect(find.text("add"), findsOneWidget);
+        },
+      );
+    },
+  );
+}
