45e56f75bd7a721cecec6cc6b181b015747b3b53
confusedalex <alex@confusedalex.dev>
2025-08-28 08:44:05 +0200

    refactor: newTagColorWidget to stateless

--- lib/features/settings/tags/new_tag_color_dialog.dart
+++ lib/features/settings/tags/new_tag_color_dialog.dart
@@ -1,57 +1,49 @@
 import 'package:calendorg/core/tag_colors/tag_color.dart';
 import 'package:calendorg/core/tag_colors/tag_colors_cubit.dart';
+import 'package:calendorg/features/settings/tags/cubit/new_tag_color_cubit.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class NewTagColorDialog extends StatefulWidget {
+class NewTagColorDialog extends StatelessWidget {
   const NewTagColorDialog({super.key});
 
   @override
-  State<NewTagColorDialog> createState() => _NewTagColorDialogState();
-}
+  Widget build(BuildContext context) {
+    final state = context.read<NewTagColorCubit>();
 
-class _NewTagColorDialogState extends State<NewTagColorDialog> {
-  String name = "";
-  Color selectedColor = Colors.blue;
-
-  @override
-  Widget build(BuildContext context) => AlertDialog(
-        title: Text("Add new Tag"),
-        content: SizedBox(
-          height: 400,
-          width: 400,
-          child: ListView(
-            children: [
-              TextField(
-                  onChanged: (value) => setState(() {
-                        name = value;
-                      })),
-              Divider(),
-              ColorPicker(
-                color: selectedColor,
-                onColorChanged: (Color color) => setState(() {
-                  selectedColor = color;
-                }),
-                pickersEnabled: <ColorPickerType, bool>{
-                  ColorPickerType.primary: false,
-                  ColorPickerType.accent: false,
-                  ColorPickerType.wheel: true
-                },
-              )
-            ],
-          ),
-        ),
-        actions: [
-          TextButton(
-              key: Key("newtag_savebutton"),
-              onPressed: () {
-                context
-                    .read<TagColorsCubit>()
-                    .addTagColor(TagColor(name, selectedColor));
-                Navigator.of(context).pop();
+    return AlertDialog(
+      title: Text("Add new Tag"),
+      content: SizedBox(
+        height: 400,
+        width: 400,
+        child: ListView(
+          children: [
+            TextField(onChanged: (value) => state.updateText(value)),
+            Divider(),
+            ColorPicker(
+              color: state.state.color,
+              onColorChanged: (Color color) => state.updateColor(color),
+              pickersEnabled: <ColorPickerType, bool>{
+                ColorPickerType.primary: false,
+                ColorPickerType.accent: false,
+                ColorPickerType.wheel: true
               },
-              child: Text("save"))
-        ],
-      );
+            )
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+            key: Key("newtag_savebutton"),
+            onPressed: () {
+              context
+                  .read<TagColorsCubit>()
+                  .addTagColor(TagColor(state.state.text, state.state.color));
+              Navigator.of(context).pop();
+            },
+            child: Text("save"))
+      ],
+    );
+  }
 }
