732eb58ca1ae90da9e0c802940043c0c73451095
confusedalex <alex@confusedalex.dev>
2025-08-29 19:51:33 +0200

    feat: add validation to newTagColor

--- lib/features/settings/tags/new_tag_color_dialog.dart
+++ lib/features/settings/tags/new_tag_color_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:calendorg/core/tag_colors/tag_color.dart';
 import 'package:calendorg/core/tag_colors/tag_colors_cubit.dart';
 import 'package:calendorg/features/settings/tags/cubit/new_tag_color_cubit.dart';
+import 'package:calendorg/util.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,39 +12,47 @@ class NewTagColorDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final state = context.read<NewTagColorCubit>();
+    final tagColorsCubit = context.read<TagColorsCubit>();
+    final formKey = GlobalKey<FormState>();
 
-    return AlertDialog(
-      title: Text("Add new Tag"),
-      content: SizedBox(
-        height: 400,
-        width: 400,
-        child: ListView(
-          children: [
-            TextField(onChanged: (value) => state.updateText(value)),
-            Divider(),
-            ColorPicker(
-              color: state.state.color,
-              onColorChanged: (Color color) => state.updateColor(color),
-              pickersEnabled: <ColorPickerType, bool>{
-                ColorPickerType.primary: false,
-                ColorPickerType.accent: false,
-                ColorPickerType.wheel: true
-              },
-            )
+    return Form(
+        key: formKey,
+        child: AlertDialog(
+          title: Text("Add new Tag"),
+          content: SizedBox(
+            height: 400,
+            width: 400,
+            child: ListView(
+              children: [
+                TextFormField(
+                    onChanged: (value) => state.updateText(value),
+                    validator: (value) => validate(value, "Tag Color",
+                        notIn: tagColorsCubit.state.map((e) => e.tag))),
+                Divider(),
+                ColorPicker(
+                  color: state.state.color,
+                  onColorChanged: (Color color) => state.updateColor(color),
+                  pickersEnabled: <ColorPickerType, bool>{
+                    ColorPickerType.primary: false,
+                    ColorPickerType.accent: false,
+                    ColorPickerType.wheel: true
+                  },
+                )
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+                key: Key("newtag_savebutton"),
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    tagColorsCubit.addTagColor(
+                        TagColor(state.state.text, state.state.color));
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: Text("save"))
           ],
-        ),
-      ),
-      actions: [
-        TextButton(
-            key: Key("newtag_savebutton"),
-            onPressed: () {
-              context
-                  .read<TagColorsCubit>()
-                  .addTagColor(TagColor(state.state.text, state.state.color));
-              Navigator.of(context).pop();
-            },
-            child: Text("save"))
-      ],
-    );
+        ));
   }
 }
