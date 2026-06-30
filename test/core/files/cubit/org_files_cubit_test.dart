a8b51be063819e3328fd07c33a0d6737c31ca3f4
confusedalex <alex@confusedalex.dev>
2026-07-01 00:48:02 +0200

    tests: org files cubit tests

--- test/core/files/cubit/org_files_cubit_test.dart
+++ test/core/files/cubit/org_files_cubit_test.dart
@@ -1,6 +1,7 @@
 import 'package:calendorg/core/files/services/org_files_repository.dart';
 import 'package:file_picker_writable/file_picker_writable.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:org_parser/org_parser.dart';
 import 'package:test/test.dart';
 import 'package:calendorg/core/files/cubit/org_files_cubit.dart';
 
@@ -8,10 +9,15 @@ class MockOrgFilesRepository extends Mock implements OrgFilesRepository {}
 
 class FakeDirectoryInfo extends Fake implements DirectoryInfo {}
 
+class FakeFileInfo extends Fake implements FileInfo {}
+
+class FakeOrgDocument extends Fake implements OrgDocument {}
+
 void main() {
   group("OrgFilesCubit", () {
     setUpAll(() {
       registerFallbackValue(FakeDirectoryInfo());
+      registerFallbackValue(FakeFileInfo());
     });
     group('setOrgDirectory()', () {
       test('should save directory in repository', () async {
@@ -36,6 +42,69 @@ void main() {
         expect(cubit.state.directory, newDirectory);
         expect(cubit.state.directory, isNotNull);
       });
+    });
+    group('addFilePath()', () {
+      test('should save file list in repository', () async {
+        final repository = MockOrgFilesRepository();
+        final cubit = OrgFilesCubit(repository);
+
+        when(
+          () => repository.loadDocument(any()),
+        ).thenAnswer((_) async => FakeOrgDocument());
+        when(() => repository.saveFileList(any())).thenAnswer((_) async {});
+        when(() => repository.parseAllEvents(any(), any())).thenReturn({});
+
+        await cubit.addFilePath(FakeFileInfo());
+
+        verify(() => repository.saveFileList(any())).called(1);
+      });
+      test(
+        'should emit new state with updated file paths and documents map',
+        () async {
+          final repository = MockOrgFilesRepository();
+          final cubit = OrgFilesCubit(repository);
+
+          final fakeFileInfo = FakeFileInfo();
+          when(
+            () => repository.loadDocument(fakeFileInfo),
+          ).thenAnswer((_) async => FakeOrgDocument());
+          when(() => repository.saveFileList(any())).thenAnswer((_) async {});
+          when(() => repository.parseAllEvents(any(), any())).thenReturn({});
+
+          await cubit.addFilePath(fakeFileInfo);
+
+          expect(cubit.state.filePaths.contains(fakeFileInfo), isTrue);
+          expect(cubit.state.documentsMap.containsKey(fakeFileInfo), isTrue);
+        },
+      );
+    });
+    group('removeFilePath()', () {
+      test('should save file list in repository', () async {
+        final repository = MockOrgFilesRepository();
+        final cubit = OrgFilesCubit(repository);
+
+        when(() => repository.saveFileList(any())).thenAnswer((_) async {});
+
+        final fakeFileInfo = FakeFileInfo();
+        await cubit.removeFilePath(fakeFileInfo);
+
+        verify(() => repository.saveFileList(any())).called(1);
+      });
+      test(
+        'should emit new state with updated file paths and documents map',
+        () async {
+          final repository = MockOrgFilesRepository();
+          final cubit = OrgFilesCubit(repository);
+
+          final fakeFileInfo = FakeFileInfo();
+          when(() => repository.saveFileList(any())).thenAnswer((_) async {});
+
+          await cubit.removeFilePath(fakeFileInfo);
+
+          expect(cubit.state.filePaths.contains(fakeFileInfo), isFalse);
+          expect(cubit.state.documentsMap.containsKey(fakeFileInfo), isFalse);
+        },
+      );
     });
   });
 }
