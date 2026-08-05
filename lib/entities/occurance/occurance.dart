22a313d8bb58df273963272f160f41f841ebf265
confusedalex <alex@confusedalex.dev>
2026-08-05 23:26:07 +0200

    occurance: implement skeleton

---
+++ lib/entities/occurance/occurance.dart
@@ -0,0 +1,24 @@
+import 'package:calendorg/entities/org_entry/org_entry.dart';
+import 'package:org_parser/org_parser.dart';
+
+enum OccurrenceKind { timestamp, scheduled, deadline }
+
+class Occurrence {
+  final OrgEntry entry;
+  final DateTime date;
+  final OccurrenceKind kind;
+  final OrgTimestamp timestamp;
+  final bool isRepeaterInstance; // true if generated, not literally in the file
+
+  Occurrence({
+    required this.entry,
+    required this.date,
+    required this.kind,
+    required this.timestamp,
+    this.isRepeaterInstance = false,
+  });
+
+  @override
+  String toString() =>
+      'Occurrence(date: $date, kind: $kind, entry: ${entry.title})';
+}
